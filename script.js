2bea9824b55274d7c5e0afe2ee6c1426a7446b08
nismith33 <nismith33@gmail.com>
2023-06-07 16:39:24 -0400

    Add slider to control number of cells per row

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const sketchpad = document.querySelector('.sketchpad');
+console.log(sketchpad)
 
 function addCells(sketchpad,cellsPerRow) {
     const cellSideLength = Math.round(Math.floor(sketchpad.clientWidth/cellsPerRow));
     for (let i = 0; i< cellsPerRow**2 ; i++) {
         const cell = document.createElement('div');
+        //the cell class name will be used for clearing the board
+        cell.className = "cell";
         cell.style.width = `${cellSideLength}px`;
         cell.style.height = `${cellSideLength}px`;
         cell.addEventListener('mouseenter', colorCell);
@@ -13,4 +16,22 @@ function addCells(sketchpad,cellsPerRow) {
 
 function colorCell() {
     this.style.backgroundColor = 'black';
+}
+
+//slider logic
+const slider = document.querySelector('.slider');
+console.log(slider);
+const sliderValue = document.querySelector('.sliderValue')
+slider.addEventListener('change', updateCells)
+
+function updateCells() {
+    removeCells(sketchpad);
+    sliderValue.textContent = this.value;
+    addCells(sketchpad,this.value);
+}
+
+function removeCells(sketchpad) {
+    while (sketchpad.firstChild) {
+        sketchpad.removeChild(sketchpad.firstChild);
+    }
 }
